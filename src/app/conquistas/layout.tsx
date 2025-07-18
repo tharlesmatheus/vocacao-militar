c6ede830cde99817b560d9485e8cd7960edc8894
Tharles Matheus <tharlesmatheus34@gmail.com>
2025-07-17 22:11:46 -0300

    feat: Rota Protegida Páginas Pessoais Dos Alunos

---
+++ src/app/conquistas/layout.tsx
@@ -0,0 +1,29 @@
+"use client";
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
+import { supabase } from "../../lib/supabaseClient"; // Caminho relativo
+
+export default function ProtectedLayout({ children }: { children: React.ReactNode }) {
+    const router = useRouter();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        supabase.auth.getUser().then(({ data }) => {
+            if (!data.user) {
+                router.replace("/auth");
+            } else {
+                setLoading(false);
+            }
+        });
+    }, [router]);
+
+    if (loading) {
+        return (
+            <div className="flex min-h-screen items-center justify-center">
+                <span className="text-lg text-[#232939]">Carregando...</span>
+            </div>
+        );
+    }
+
+    return <>{children}</>;
+}
